991199034a6ced762824635dec11c8b607ef041a
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2020-08-25 01:14:42 +0700

    lang

--- src/app/config/default.js
+++ src/app/config/default.js
@@ -141,11 +141,11 @@ export const APP_OPTION = {
 }
 export const LOCALE_MAPPING_FLAG_ICON_NAME_DEF = {
     en: 'gb',
-    ja: 'jp',
+    vi: 'vi',
 }
 export const LOCALE_MAPPING_MOMENT_DEF = {
     en: 'en-gb',
-    ja: 'ja',
+    vi: 'vi',
 }
 const maxChunkUploadSize = parseInt(process.env.VUE_APP_MAX_CHUNK_UPLOAD_SIZE) | 0
 export const MAX_CHUNK_UPLOAD_SIZE = maxChunkUploadSize ? maxChunkUploadSize : 1024 * 1024 // 1 MB, default
